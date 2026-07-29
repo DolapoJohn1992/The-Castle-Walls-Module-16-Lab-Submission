757764978c22fb7523d2f1c36aaef3550efcfd89
admin <admin@MacBookPro.lan>
2026-07-29 16:34:47 -0400

    feat: complete Module 16 lab submission

--- vpc_architecture.tf
+++ vpc_architecture.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # The Main VPC
 resource "aws_vpc" "tkh_fortress" {
   cidr_block = "10.0.0.0/16"
-  
+
   tags = {
     Name = "TKH-Fortress-VPC"
   }
@@ -16,13 +16,28 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.tkh_fortress.id
 }
 
+# The Public Subnet (Added)
+resource "aws_subnet" "public_courtyard" {
+  vpc_id                  = aws_vpc.tkh_fortress.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_courtyard"
+  }
+}
+
 # The Private Subnet (The Vault)
 resource "aws_subnet" "private_vault" {
   vpc_id     = aws_vpc.tkh_fortress.id
   cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private_vault"
+  }
 }
 
-# The Route Table
+# The Public Route Table
 resource "aws_route_table" "corrupted_route" {
   vpc_id = aws_vpc.tkh_fortress.id
 
@@ -32,8 +47,8 @@ resource "aws_route_table" "corrupted_route" {
   }
 }
 
-# SABOTAGE: The Private Subnet is associated with the Internet Route Table!
-resource "aws_route_table_association" "sabotage_association" {
-  subnet_id      = aws_subnet.private_vault.id
+# Route Table Association (Fixed: Attached to public_courtyard instead of private_vault)
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_courtyard.id
   route_table_id = aws_route_table.corrupted_route.id
 }
